710d0d84f0a6fabe941d039dc219300a92ca65a1
Sofiia <sofiia@MacBook-Air-Sofiia.local>
2024-04-02 16:04:45 +0200

    person

---
+++ src/info/person.jsx
@@ -0,0 +1,36 @@
+export const person = [
+    {
+    "name":"Призрачный гонщик",
+    "universe":"Marvel",
+    "intellegence": "Высокий",
+    "force":"Демоническая трансформация",
+    "speed":"Очень быстрый",
+    "skills":"Навыки езды",
+    "url": "https://avatars.dzeninfra.ru/get-zen_doc/10349846/pub_649c1f4add23774f7203b128_649c1f905a71a1676e92186c/scale_1200"
+    },
+    
+    {
+        "name":"Человек-паук",
+        "universe":"Marvel Comics",
+        "intellegence": "Высокий",
+        "force":"Трансформация",
+        "speed":"Очень быстрый",
+        "skills":"Наличие паутины",
+        "url": "https://icdn.lenta.ru/images/2019/08/21/09/20190821094242757/detail_1bd0eeff4f314cf7d269d589889295de.jpg"
+        },
+    
+        {
+            "name":"Алая Ведьма",
+            "universe":"Marvel Comics",
+            "intellegence": "Высокий",
+            "force":"экстрасенсорными способностями",
+            "speed":"Очень быстрый",
+            "skills":"Телекинез",
+            "url": "https://www.soyuz.ru/public/uploads/files/2/7612751/202209291256082d60befe3a.jpg"
+            }
+    
+    ]
+    
+    
+    
+    
